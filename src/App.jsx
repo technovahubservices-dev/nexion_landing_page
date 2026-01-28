a50888c2dc483179461a86c8b61afa0afd05da95
technovahubservices-dev <technovahubservices@gmail.com>
2026-01-28 11:47:22 +0530

    Fix: remove bootstrap import from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { Navbar } from './Navbar.jsx';
 import { Features } from './Features.jsx';
 import { UseCases } from './UseCases.jsx';
