19b6bf9bfc90f5045b963689c11fdb869181fc03
technovahubservices-dev <technovahubservices@gmail.com>
2026-01-28 15:11:55 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-import { Navbar } from './Navbar.jsx';
-import { Features } from './Features.jsx';
-import { UseCases } from './UseCases.jsx';
-import { Pricing } from './Pricing.jsx';
-import { ScrollToTop } from './ScrollToTop.jsx';
-import { FloatingCTA } from './FloatingCTA.jsx';
-import { Contact } from './Contact.jsx';
-
+import { Navbar } from "./Navbar.jsx";
+import { Features } from "./Features.jsx";
+import { UseCases } from "./UseCases.jsx";
+import { Pricing } from "./Pricing.jsx";
+import { ScrollToTop } from "./ScrollToTop.jsx";
+import { FloatingCTA } from "./FloatingCTA.jsx";
+import { Contact } from "./Contact.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-       <Navbar/>
-       <div className="main-content">
-        <Features/>
-       </div>
-       <UseCases/>
-       <Pricing/>
-       <ScrollToTop/>
-       <FloatingCTA/>
-       <Contact/>
+      <Navbar />
+      <div className="main-content">
+        <Features />
+      </div>
+      <UseCases />
+      <Pricing />
+      <ScrollToTop />
+      <FloatingCTA />
+      <Contact />
+      <footer className="footer">
+        © 2026 <span>TechnovaHub</span>. All Rights Reserved.
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
